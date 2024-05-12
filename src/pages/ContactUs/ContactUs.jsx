34594f3c31a068adab33ac9e36938fd4beee9e13
achrafbenmed <achrafbenmohamed99@gmail.com>
2024-05-12 14:41:08 +0200

    version final

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -12,21 +12,27 @@ const ContactUs = () => {
       >
         Contacter Nous
       </Typography>
-      <Stack direction={"row"} sx={{ marginTop: 20 }} spacing={2}>
+      <Stack direction={"row"} sx={{ marginTop: 13 }} spacing={2}>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10579.16107992883!2d10.091276469307411!3d33.88160205448069!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12556fbba0f54ebd%3A0x94ae80792469630e!2sCamp%20Militaire%2C%20Gab%C3%A8s!5e0!3m2!1sfr!2stn!4v1715476203514!5m2!1sfr!2stn"
-          width="800"
-          height="600"
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d828.092517832784!2d10.091896545727602!3d33.88012171694626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzPCsDUyJzQ2LjAiTiAxMMKwMDUnMzEuOSJF!5e0!3m2!1sen!2stn!4v1715517197983!5m2!1sen!2stn"
+          width="600"
+          height="450"
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
         <Stack spacing={2}>
-          <Typography textAlign={"left"}>Tel : </Typography>
+          <Typography textAlign={"left"}>Tel : 22036176</Typography>
           <Typography textAlign={"left"}>
-            E-mail :<a href="mailto:email">email</a>
+            E-mail :
+            <a href="mailto:achrafbenmohamed99@gmail.com">
+              achrafbenmohamed99@gmail.com
+            </a>
           </Typography>
-          <Typography textAlign={"left"}>Adresse : </Typography>
+          <Typography textAlign={"left"}>
+            Adresse :جامع Avenue Béchir El Jaziri - شارع البشير الجزيري, Gabès
+            6000
+          </Typography>
         </Stack>
       </Stack>
     </>
